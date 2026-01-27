b8059d4fd0f1498e9b6833d0fb2e68bb4bfb1f99
veselinov.viktor <veselinov.viktor@icloud.com>
2026-01-27 15:32:49 +0200

    adding new fixes

--- src/main/java/com/company/ticket_service/core/config/CacheConfig.java
+++ src/main/java/com/company/ticket_service/core/config/CacheConfig.java
@@ -9,6 +9,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -20,6 +21,7 @@ import java.time.Duration;
 
 @Configuration
 @EnableCaching
+@Profile("!test")
 public class CacheConfig {
 
   @Bean
