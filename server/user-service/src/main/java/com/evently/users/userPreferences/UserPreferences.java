4a3482dc71252f1843732592158a72f0bf735f02
veselinov.viktor <veselinov.viktor@icloud.com>
2026-02-03 11:12:13 +0200

    added missing pom.xml dependency to user-service

--- server/user-service/src/main/java/com/evently/users/userPreferences/UserPreferences.java
+++ server/user-service/src/main/java/com/evently/users/userPreferences/UserPreferences.java
@@ -1,7 +1,10 @@
 package com.evently.users.userPreferences;
 
 import com.evently.users.user.User;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import utils.BaseEntity;
